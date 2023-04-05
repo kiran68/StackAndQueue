fd025ca9e1ab9a788a5618acd9d7ce5dd453373e
kiran68 <kiranurshil88@gmail.com>
2023-04-05 17:42:48 +0530

    Add: Uding dqueue In Program

--- StackAndQueueDemo.java
+++ StackAndQueueDemo.java
@@ -1,27 +1,24 @@
 package StackAndQueueDemo;
 
-import java.util.EmptyStackException;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 public class StackAndQueueDemo {
 
 	public static void main(String[] args) {
-	
+
 		Queue<Integer> queue = new LinkedList<>();
-		
+
 		queue.add(56);
 		queue.add(30);
 		queue.add(70);
-		
+
 		System.out.println("The Queue elements are : " + queue);
 		System.out.println("The Peek Of The Element :" + queue.peek());
-		
-		
 
-		
-		 
+		int number = queue.remove();
+		System.out.println("After Removing the Element : " + number);
+		System.out.println("The Queue elements are : " + queue);
 
 	}
 
